f1e09cc90f5a2c3df0231c023bc2b36c10035c50
Alain - VE2CUY <alain.boudreault@me.com>
2021-05-07 16:32:24 -0400

    Fichiers de départ

---
+++ Dockerfile
@@ -0,0 +1,2 @@
+FROM php:8.0.3-apache-buster
+COPY . /var/www/html/
---
+++ index.php
@@ -0,0 +1,137 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"  "http://www.w3.org/TR/html4/strict.dtd">
+<!--[if IE 6]>
+<html id="ie6" lang="fr-CA">
+<![endif]-->
+<!--[if IE 7]>
+<html id="ie7" lang="fr-CA">
+<![endif]-->
+<!--[if IE 8]>
+<html id="ie8" lang="fr-CA">
+<![endif]-->
+<!--[if !(IE 6) | !(IE 7) | !(IE 8)  ]><!-->
+<html lang="fr-CA">
+<!--<![endif]-->
+<head profile="http://gmpg.org/xfn/11">
+    <meta http-equiv="content-type" content="text/html;chartset=UTF-8"/>
+    <meta charset="UTF-8">
+    <title>420-4D4 App de départ</title>
+    <script type="text/javascript">
+    </script>
+    <style type="text/css">
+    img.wp-smiley, img.emoji {
+        display: inline !important;
+        border: none !important;
+        box-shadow: none !important;
+        height: 1em !important;
+        width: 1em !important;
+        margin: 0 .07em !important;
+        vertical-align: -0.1em !important;
+        background: none !important;
+        padding: 0 !important;
+    }
+    </style>
+    <link rel='stylesheet' id='wp-block-library-css' href='css/style.min.css' type='text/css' media='all'/>
+    <link rel='stylesheet' id='simplestyle_style-css' href='css/style.css' type='text/css' media='all'/>
+ </head>
+<body class="home page-template-default page page-id-5">
+
+    <div id="header">
+        <a class="title" href="http://ve2cuy.com">
+            <div id="title">420-4D4 : Épreuve synthèse</div>
+        </a>
+    </div>
+
+    <div id="nav-container">
+        <div id="nav">
+            <div class="menu">
+                <div class="menu-menu-1-container">
+                    <ul id="menu-menu-1" class="nav">
+                        <li id="menu-item-26" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-26">
+                            <a href="#">Item1</a>
+                        </li>
+                        <li id="menu-item-27" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-27">
+                            <a href="#">Item2</a>
+                        </li>
+                        <li id="menu-item-28" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-28">
+                            <a href="http://ve2cuy.com/420-4d4b/index.php/planification-hebdomadaire/">420-4D4</a>
+                        </li>
+                        <li id="menu-item-56" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-56">
+                            <a href="#">Énoncé E.S.</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="clear"></div>
+
+    <div id="container">
+
+        <div class="article" class="post-5 page type-page status-publish hentry">
+            <h1>
+                <a href="http://ve2cuy.com/">Déploiement K8s - version:es-1.0</a>
+            </h1>
+            <div class="date"></div>
+
+            <p class="has-text-align-center">
+                <img loading="lazy" width="520" height="271" class="wp-image-58" src="tableau-k8s.png" alt="" style="width: 500px;" sizes="(max-width: 520px) 100vw, 520px"/>
+            </p>
+
+            <hr class="wp-block-separator"/>
+
+
+            <p></p>
+
+            <h2>
+                <span class="has-inline-color has-pale-pink-color">Vous avez été servi par le POD : </span>Inscrire ici le code php
+                <br/><span class="has-inline-color has-luminous-vivid-amber-color">Version du serveur web : </span>Inscrire ici le code php
+            </h2>
+
+            <hr class="wp-block-separator"/>
+            <h2>
+                <span class="has-inline-color has-vivid-purple-color">Liste des applications en déploiement K8s</span>
+            </h2>
+
+            <p>
+                <a href="#">Ce site Web (12 duplicatas)</a>
+                <br/>
+                <a href="#">Item1</a>
+                <br/>
+                <a href="#">Item2</a>
+
+            <hr class="wp-block-separator"/>
+
+            <h2>
+                <br>
+                <span class="has-inline-color has-luminous-vivid-amber-color">
+                    À remettre avant le: &nbsp
+                    <a href="http://ve2cuy.com/420-4d4b/index.php/epreuve-synthese/">2021.05.25 23:59</a>
+                </span>
+            </h2>
+
+            <p></p>
+
+            <hr class="wp-block-separator"/>
+
+            <h6 class="has-text-align-right">
+                <span style="color:#0072a3" class="has-inline-color">App par 'Votre nom' &#8211; version docker votreCompte/420-4d4-es:es-1.0</span>
+            </h6>
+
+            <div class="clear"></div>
+
+            <div id="comments">
+                <div class="show-comments">
+                    <div class="commentline"></div>
+                    <div class="countcomments"></div>
+
+                    <div class="commentform">
+                        <div style="clear:both;"></div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <div id="border-bottom"></div>
+
+</body>
+</html>
