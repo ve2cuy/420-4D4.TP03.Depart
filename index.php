8d503db570c5a133dddb3e486afebbba9f21a2e9
ve2cuy <alain.boudreault@me.com>
2022-05-06 09:44:28 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
-echo "<center><h1>Ici la voix des Mistérons!!!</h1></center>";
+echo "<hr/><center><h1>Ici la voix des Mistérons!!!</h1></center><hr/>";
 
-echo "<h2>Il ne faut pas afficher cette page dans la version finale de votre projet!</h2>";
+echo "<center><h2>Il ne faut pas afficher cette page dans la version finale de votre projet!</h2></center>";
 
 ?>
